67a3c9eae818aaceb8694db88ac51bddfd3dbae7
haguui <1043398@qq.com>
2017-11-21 17:50:24 +0800

    jwt

---
+++ src/main/java/com/yonyou/cloud/common/jwt/JWTHelper.java
@@ -0,0 +1,63 @@
+package com.yonyou.cloud.common.jwt;
+
+import org.joda.time.DateTime;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+/**
+ * Created by ace on 2017/9/10.
+ */
+public class JWTHelper {
+    public static final String JWT_KEY_USER_ID = "userId";
+    public static final String JWT_KEY_NAME = "name";
+    private static RsaKeyHelper rsaKeyHelper = new RsaKeyHelper();
+    /**
+     * 密钥加密token
+     *
+     * @param jwtInfo
+     * @param priKeyPath
+     * @param expire
+     * @return
+     * @throws Exception
+     */
+    public static String generateToken(IJWTInfo jwtInfo, String priKeyPath, int expire) throws Exception {
+        String compactJws = Jwts.builder()
+                .setSubject(jwtInfo.getUniqueName())
+                .claim(JWT_KEY_USER_ID, jwtInfo.getId())
+                .claim(JWT_KEY_NAME, jwtInfo.getName())
+                .setExpiration(DateTime.now().plusSeconds(expire).toDate())
+                .signWith(SignatureAlgorithm.RS256, rsaKeyHelper.getPrivateKey(priKeyPath))
+                .compact();
+        return compactJws;
+    }
+
+    /**
+     * 公钥解析token
+     *
+     * @param token
+     * @return
+     * @throws Exception
+     */
+    public static Jws<Claims> parserToken(String token, String pubKeyPath) throws Exception {
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(rsaKeyHelper.getPublicKey(pubKeyPath)).parseClaimsJws(token);
+        return claimsJws;
+    }
+
+    /**
+     * 获取token中的用户信息
+     *
+     * @param token
+     * @param pubKeyPath
+     * @return
+     * @throws Exception
+     */
+    public static IJWTInfo getInfoFromToken(String token, String pubKeyPath) throws Exception {
+        Jws<Claims> claimsJws = parserToken(token, pubKeyPath);
+        Claims body = claimsJws.getBody();
+        return new JWTInfo(body.getSubject(), StringHelper.getObjectValue(body.get(JWT_KEY_USER_ID)), StringHelper.getObjectValue(body.get(JWT_KEY_NAME)));
+    }
+
+}
