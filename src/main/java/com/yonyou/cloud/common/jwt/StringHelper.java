bd14d86b0a3557c4ba82879cc02299c9842edf7e
haguui <1043398@qq.com>
2017-12-13 17:22:20 +0800

    common

--- src/main/java/com/yonyou/cloud/common/jwt/StringHelper.java
+++ src/main/java/com/yonyou/cloud/common/jwt/StringHelper.java
@@ -1,10 +1,12 @@
 package com.yonyou.cloud.common.jwt;
 
-/**
- * Created by ace on 2017/9/10.
- */
 public class StringHelper {
     public static String getObjectValue(Object obj){
         return obj==null?"":obj.toString();
+    }
+    public static boolean isNullOrEmpty(String str){
+    	if(str==null || "".equals(str))
+    		return true;
+    	return false;
     }
 }
