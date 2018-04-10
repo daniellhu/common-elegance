b5a7d29247af09e3b3716797dab128a95b2d2e57
liuxudong-yonyou <dong1987>
2018-04-10 12:05:32 +0800

    notcc

--- src/main/java/com/yonyou/cloud/common/tcc/TransactionEntity.java
+++ src/main/java/com/yonyou/cloud/common/tcc/TransactionEntity.java
@@ -1,44 +1,44 @@
-package com.yonyou.cloud.common.tcc;
-
-import java.io.Serializable;
-
-import org.mengyun.tcctransaction.api.TransactionContext;
-
-public class TransactionEntity<T> implements Serializable {
-
-    private static final long serialVersionUID = -5183807142356024818L;
-
-    /** 事务上下文 */
-    private TransactionContext context;
-
-    /** 内容体 */
-    private T body;
-
-    public TransactionEntity() {
-    }
-
-    public TransactionEntity(TransactionContext context) {
-        this.context = context;
-    }
-
-    public TransactionEntity(TransactionContext context, T body) {
-        this.context = context;
-        this.body = body;
-    }
-
-    public TransactionContext getContext() {
-        return context;
-    }
-
-    public void setContext(TransactionContext context) {
-        this.context = context;
-    }
-
-    public T getBody() {
-        return body;
-    }
-
-    public void setBody(T body) {
-        this.body = body;
-    }
-}
+//package com.yonyou.cloud.common.tcc;
+//
+//import java.io.Serializable;
+//
+//import org.mengyun.tcctransaction.api.TransactionContext;
+//
+//public class TransactionEntity<T> implements Serializable {
+//
+//    private static final long serialVersionUID = -5183807142356024818L;
+//
+//    /** 事务上下文 */
+//    private TransactionContext context;
+//
+//    /** 内容体 */
+//    private T body;
+//
+//    public TransactionEntity() {
+//    }
+//
+//    public TransactionEntity(TransactionContext context) {
+//        this.context = context;
+//    }
+//
+//    public TransactionEntity(TransactionContext context, T body) {
+//        this.context = context;
+//        this.body = body;
+//    }
+//
+//    public TransactionContext getContext() {
+//        return context;
+//    }
+//
+//    public void setContext(TransactionContext context) {
+//        this.context = context;
+//    }
+//
+//    public T getBody() {
+//        return body;
+//    }
+//
+//    public void setBody(T body) {
+//        this.body = body;
+//    }
+//}
